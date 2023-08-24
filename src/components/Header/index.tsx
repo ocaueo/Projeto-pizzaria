9d0ab669859c91f90d6e6943e3cc20ec8c61eb69
unknown <08827805303@DIGITALCOLLEGE.LOCAL>
2023-08-24 10:15:36 -0300

    enviando o projeto

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,42 @@
+import { ShoppingCart } from "@phosphor-icons/react";
+import { useState } from "react";
 import { styled } from "styled-components";
 
 const Header = () => {
+
+const [cartVisible, setCartVisible] = useState<boolean>(false);
+
+
     return(
         <>
             <HeaderContainer>
+                <HeaderActions>
                 <HeaderLogo>LOGO</HeaderLogo>
-                <HeaderBTN href="/login">Login</HeaderBTN>
-            </HeaderContainer>
+                <HeaderMenu>
+                    <li>
+                        <a href="/">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="/cardapio">Cardápio</a>
+                    </li>
+                    <li>
+                        <a href="/promocoes">Promoções</a>
+                    </li>
+                </HeaderMenu>
+                </HeaderActions>
+                
+                
+                <HeaderActions>
+                    <HeaderBTN href="/login">Login</HeaderBTN>
+                    <ShoppingCart className="icon" size={24} 
+                    onClick={() => setCartVisible(!cartVisible)}
+                    />
+                    <HeaderCart className={cartVisible ? "active" : ""}>
+
+                    </HeaderCart>
+                </HeaderActions>
+
+                </HeaderContainer>
         </>
     );
 }
@@ -18,12 +48,40 @@ const HeaderContainer = styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
+
+    & .icon {
+        width: 50px;
+        height: 50px;
+        cursor: pointer;
+        border-radius: 25px;
+        background-color: #fff;
+        padding: 12px;
+    }
 `;
 
 const HeaderLogo = styled.h1`
     font-size: 44px;
     font-weight: bold;
     color: #FFFFFF;
+`;
+
+const HeaderMenu = styled.ul`
+    display: flex;
+    gap: 16px;
+
+    & li a {
+        font-size: 16px;
+        color: #ffffff;
+        line-height: 40px;
+        display: block;
+    }
+`;
+
+const HeaderActions = styled.div`
+    display: flex;
+    gap: 16px;
+    align-items: center;
+    position: relative;
 `;
 
 const HeaderBTN = styled.a`
@@ -37,6 +95,33 @@ const HeaderBTN = styled.a`
     text-transform: uppercase;
     font-size: 12px;
     font-weight: bold;
+`;
+
+const HeaderCart = styled.div`
+    width: 250px;
+    height: 300px;
+    background-color: #ffff00;
+    position: absolute;
+    top: calc(100% + 16px);
+    right: 0;
+    border-radius: 5px;
+    visibility: hidden;
+    transition: all 200ms;
+
+    &.active {
+        opacity: 1;
+        visibility: visible;
+    }
+
+    &::before {
+    content: "";
+    border: 6px solid transparent;
+    border-top: 0;
+    border-bottom-color: #ffff00;
+    position: absolute;
+    bottom: 100%;
+    right: 22px;
+    }
 `;
 
 export default Header;
